635131377a3f7debc1c1e62cc3e98c6dc201fec9
darkwood23 <anonymoushackerintheweb@gmail.com>
2023-02-24 20:29:12 +0545

    All done!

--- script.js
+++ script.js
@@ -6,8 +6,20 @@ function Book() {
 
 }
 
+function removeEverything() {
+    const books = document.querySelector(".books")
+    const author = document.querySelector('#book-writer')
+    const title = document.querySelector("#book-name")
+    author.value = ""
+    title.value = ""
+    books.textContent = ""
+}
+
 function addBookToLibrary() {
-    
+    const title = document.querySelector("#book-name").value
+    myLibrary.push(title.toString())
+    removeEverything()
+    displayBooks()
 }
 
 function displayForm() {
@@ -16,6 +28,14 @@ function displayForm() {
     } else {
         onlyForm.style.display = "none"
     }
+}
+
+function deleteBook(a) {
+    console.log(a)
+    myLibrary.splice(a, 1)
+    const books = document.querySelector(".books")
+    books.textContent = ""
+    displayBooks()
 }
 
 function displayBooks() {
@@ -31,12 +51,14 @@ function displayBooks() {
 
         alreadyRead.type = 'checkbox'
         removeBookButton.classList.add('remove-button')
+        removeBookButton.setAttribute("id", "remove" + i.toString())
         contentHolder.classList.add('content-holder' + i.toString())
         contentHolder.classList.add('content-holder')
         pTag.classList.add('book' + i.toString())
         pTag.textContent = myLibrary[i]
         labelTag.textContent = 'Already Read:'
         removeBookButton.textContent = 'Remove Book'
+        removeBookButton.setAttribute('onclick', `deleteBook(${i})`)
 
         placeHolder.appendChild(labelTag)
         placeHolder.appendChild(alreadyRead)
